8bb9023477281e9575cdc11cefdd662445dd6e54
guoyong <guo-yong123@163.com>
2019-11-30 19:43:27 +0800

    init commit

---
+++ spring-cloud-security-oauth2-server/src/main/java/com/tl/oauth2/OAuth2ServerApplication.java
@@ -0,0 +1,16 @@
+package com.tl.oauth2;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author guoyong
+ */
+@SpringBootApplication
+public class OAuth2ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ServerApplication.class,args);
+    }
+}
---
+++ spring-cloud-security-oauth2-server/src/main/java/com/tl/oauth2/config/WebSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.tl.oauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author guoyong
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 设置默认的加密方式
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().
+                withUser("admin").password(passwordEncoder().encode("123456")).roles("ADMIN");
+    }
+}
